2892185e1ffa63f0cec1b87c9444810ac2e9f778
BIDIGA Imrane <kouewanbidiga2@gmail.com>
2025-11-25 15:46:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # aqua-optim-energy
 # Supprimez l'ancien
 # Créez le nouveau avec le code complet
-echo plotly>=5.15.0 >> requirements.txt
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
 def main():
